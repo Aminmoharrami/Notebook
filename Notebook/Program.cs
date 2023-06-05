1938d49e33078bcefdddb27303dc397fb44698a2
Aminmoharrami <58686938+Aminmoharrami@users.noreply.github.com>
2023-06-05 13:17:45 +0330

    Notebook 1402/03/15 1.Program.cs 2.Startup 3.lunchSetting

--- Notebook/Program.cs
+++ Notebook/Program.cs
@@ -1,4 +1,4 @@
-var builder = WebApplication.CreateBuilder(args);
+﻿var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -22,3 +22,10 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
+
+//Program.cs
+//وظیفه اجرای برنامه را بر عهده دارد و اینکه با توجه به محیطی که در آن کار میکنیم تنظیمات مربوط به بالا اومدن اپلیکیشن مارا انجام میدهد
+
+
+//Startup.cs
+//این فایل در دانت نت 6 حذف و به لطف C# 10 و امکان top level statement تمام تنظیمات در همین فایل program.cs انجام شده
